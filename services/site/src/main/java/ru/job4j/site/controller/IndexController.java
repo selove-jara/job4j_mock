7255ec344fe7ec4bc1ffea0935a3370a81fb2687
selov <selove.jara@gmail.com>
2024-12-23 17:21:59 +0400

    4. Микросервисные системы [#338434]

--- services/site/src/main/java/ru/job4j/site/controller/IndexController.java
+++ services/site/src/main/java/ru/job4j/site/controller/IndexController.java
@@ -6,12 +6,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.job4j.site.service.AuthService;
-import ru.job4j.site.service.CategoriesService;
-import ru.job4j.site.service.InterviewsService;
-import ru.job4j.site.service.NotificationService;
+import ru.job4j.site.dto.ProfileDTO;
+import ru.job4j.site.service.*;
 
 import javax.servlet.http.HttpServletRequest;
+
+import java.util.Optional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static ru.job4j.site.controller.RequestResponseTools.getToken;
 
@@ -23,6 +25,7 @@ public class IndexController {
     private final InterviewsService interviewsService;
     private final AuthService authService;
     private final NotificationService notifications;
+    private final ProfilesService profilesService;
 
     @GetMapping({"/", "index"})
     public String getIndexPage(Model model, HttpServletRequest req) throws JsonProcessingException {
@@ -41,7 +44,14 @@ public class IndexController {
         } catch (Exception e) {
             log.error("Remote application not responding. Error: {}. {}, ", e.getCause(), e.getMessage());
         }
-        model.addAttribute("new_interviews", interviewsService.getByType(1));
+        var interviewDTO = interviewsService.getByType(1);
+        List<ProfileDTO> userList = interviewDTO.stream()
+                .map(interview -> profilesService.getProfileById(interview.getSubmitterId()))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.toList());
+        model.addAttribute("new_interviews", interviewDTO);
+        model.addAttribute("users", userList);
         return "index";
     }
 }
